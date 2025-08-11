224401034a1f4909b371a08a5b6c22af9df7f0e7
Vishruth <emailvishruth@gmail.com>
2025-08-12 04:11:26 +0530

    feat: contact model

---
+++ src/models/contact.ts
@@ -0,0 +1,24 @@
+export interface Contact {
+  id: number;
+  phoneNumber?: string | null;
+  email?: string | null;
+  linkedId?: number | null;
+  linkPrecedence: 'primary' | 'secondary';
+  createdAt: string;
+  updatedAt: string;
+  deletedAt?: string | null;
+}
+
+export interface IdentifyRequest {
+  email?: string | null;
+  phoneNumber?: string | null;
+}
+
+export interface IdentifyResponse {
+  contact: {
+    primaryContatctId: number;
+    emails: string[];
+    phoneNumbers: string[];
+    secondaryContactIds: number[];
+  };
+}
